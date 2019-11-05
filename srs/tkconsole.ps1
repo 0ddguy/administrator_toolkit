709a32f637aa891d81dac1956859de98c7117490
disastrpc <jaredfreed@gmail.com>
2019-11-05 17:42:05 -0500

    working parser!

--- srs/tkconsole.ps1
+++ srs/tkconsole.ps1
@@ -1,76 +1,117 @@
 using namespace System;
 using namespace Systems.Collections.Generic
-# Import-Module ActiveDirectory
+# import-module activedirectory
+$PSver = $PSversiontable.PSversion
+$dom = get-addomain -current localcomputer
+
+class DomainHandler
+{
+    [void]check([string]$filter, [string]$id)
+    {
+
+    }
+}
+
+# Contains lists and actions used in ArgumentParser
+class ArgumentContainer
+{
+    [System.Collections.ArrayList]$arg_lst
+    [System.Collections.ArrayList]$arg_lst_to_consume
+    [string]$arg_str
+    [hashtable]$namespace = @{}
+}
+
+# Parses arguments and adds them to namespace hashtable. 
+class ArgumentParser : ArgumentContainer
+{
+    # get argument values with -
+    [string]$RX_OPT_ARG = "^-{1}[a-zA-Z0-9]+"
+    # get argument values with --
+    [string]$RX_OPT_LARG = "^-{2}[a-zA-Z0-9]+"
+    # detect strings in quotes
+    [string]$RX_QUOTE = "([^""]*)"
+    # bad characters not to be used in args
+    [string]$RX_BAD_CHARS = "[\*\?\]\[\^\+\`\!\@\#\$\%\&]"
+
+
+    [void]parse_args()
+    {
+        $this.arg_str = $this.get_arg_str()
+        $this.arg_lst = $this.arg_str.split(' ')
+
+        if($this.arg_lst[0] -match $this.RX_OPT_ARG -or $this.arg_lst[0] -match $this.RX_OPT_LARG)
+        {
+            throw '[ERROR] ' + $this.arg_lst[0] + ' must be a mode switch'
+        }
+
+        # initialize pointers
+        [int]$p1 = 0
+        [int]$p2 = 0
+
+        # iter through argument list
+        foreach($v in $this.arg_lst)
+        {
+            # mode argument will always be at index 0
+            if($p1 -eq 0)
+            {
+                $this.namespace.mode = $v
+                $p1++
+                continue
+            }
+
+
+            # match arguments to values and add to namespace
+            if($v -match $this.RX_OPT_ARG -or $v -match $this.RX_OPT_LARG)
+            {
+                # detect if argument contains a string value surrounded by quotations
+                if($this.arg_lst[$p1 + 1].startswith('"'))
+                {
+                    # set p1 to p2
+                    $p2 = $p1
+
+                    # increment p2 until the other quote is found
+                    do {$p2++} until ($this.arg_lst[$p2].endswith('"'))
+
+                    # move pointer to beginning of string
+                    $p1 += 1
+
+                    # string value is between $p1 + 1 and $p2, assign to namespace
+                    $this.namespace[$v] = $this.arg_lst[$p1..$p2]
+
+                    # set p1 back
+                    $p1 -= 1
+                }
+
+                else
+                {
+                    # assign value to namespace
+                    $this.namespace[$v] = $this.arg_lst[$p1 + 1]
+                }
+            }
+            $p1++
+        }
+    }
+
+    # get input from user
+    [string]get_arg_str() 
+    {
+        $h = get-host
+        $this.arg_str = $h.UI.readline()
+        return $this.arg_str
+    }
+
+}
 
 function main
 {
     $parser = [ArgumentParser]::new()
-    $parser.add_argument('-a')
-    $parser.add_argument('-b')
-    $parser.add_argument('-c')
-    $parser.add_argument('--argument')
-    $parser.add_argument('--argument2')
-    write-host $parser.args_to_parse
-    write-host $parser.largs_to_parse
+    do
+    {
+    write-host "tkconsole>" -nonewline
+    $parser.parse_args()
+    write-host $parser.namespace.item('-a')
+    } until ($parser.namespace.mode -eq 'quit')
 }
-
-class ArgumentContainer
-{
-    [System.Collections.ArrayList]$arg_lst
-    [string]$arg_str
-    [string]$prefix
-    [string]$action
-    [System.Collections.ArrayList]$args_to_parse = @()
-    [System.Collections.ArrayList]$largs_to_parse = @()
-    [hashtable]$namespace = @{}
-}
-
-class ArgumentParser : ArgumentContainer
-{
-    # Regular expression to match any string that begins with a - character or 
-    # by the -- followed by any number of characters
-    [string]$RX_OPT_ARG = "^-{1}[a-zA-Z0-9]+"
-    [string]$RX_OPT_LARG = "^-{2}[a-zA-Z0-9]+"
-    [string]$RX_ARG = ""
-
-    # use rx and match args and long args. arguments to parse for get added to ArgumentContainer.
-    [void]add_optional_argument([string]$arg, [string]$dest, [bool]$required=$false)
-    {
-        if($arg -match $this.RX_OPT_ARG)
-        {
-            $this.args_to_parse.add($arg)
-        }
-
-        elseif($arg -match $this.RX_OPT_LARG)
-        {
-            $this.largs_to_parse.add($arg)
-        }
-
-    }
-    
-    [void]add_argument([string]$arg)
-    {
-
-    }
-
-
-    # get input from user and return
-    [string]get_arg_string() 
-    {
-        $h = get-host
-        $this.arg_str = $h.UI.readline()
-        return $this.container.arg_str
-    }
-
-    [System.Collections.ArrayList]parse_str_args()
-    {
-
-        return $Null
-    }
-
-
-}
-
 
 main
 
